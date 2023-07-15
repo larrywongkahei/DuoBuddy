45da894ee7faa3b521dd2f7d659a53219c993d73
Maryhei <makemak123@gmail.com>
2023-07-15 12:35:30 +0100

    Added comments on apiservice file and app file

--- src/APIService.js
+++ src/APIService.js
@@ -30,6 +30,8 @@ const APIService =
       this.createUser(name, bio, location, avatar_url, email)
     }
   },
+
+  // Post project to database
   createProject(payload) {
     return fetch("http://localhost:8080/project",
       {
@@ -37,6 +39,9 @@ const APIService =
         body: JSON.stringify(payload), headers: { 'Content-Type': 'application/json' }
       })
   },
+
+  // Update user detail, input bio, location or projects in param field
+  // for project, input project id into the body
   updateUser(userId, param, payload) {
     return fetch(`http://localhost:8080/user/${userId}/${param}`,
       { method: "PUT", body: JSON.stringify(payload), headers: { 'Content-Type': 'application/json' } })
@@ -46,10 +51,12 @@ const APIService =
     return fetch("http://localhost:8080/project")
       .then(response => response.json())
   },
+
   getProjectById(id) {
     return fetch(`http://localhost:8080/project/${id}`)
       .then(response => response.json())
   },
+  // searchData could be tags or title
   getProjectsBySearch(searchData) {
     return fetch(`http://localhost:8080/project/getSearchResult?searchData=${searchData}`)
       .then(response => response.json())
@@ -58,6 +65,8 @@ const APIService =
     return fetch(`http://localhost:8080/user/dataFetching?id=${id}`)
       .then(response => response.json())
   },
+
+  // Add comment to project
   commentProject(projectId, content) {
     return fetch(`http://localhost:8080/project/${projectId}?userId=${sessionStorage.getItem('userId')}`, {
       method: "PUT",
@@ -65,6 +74,8 @@ const APIService =
       headers: { 'Content-Type': 'application/json' }
     })
   },
+
+  // Fetch data from google oauth api
   async FetchDataFromGoogle(token) {
     const response = await fetch("https://www.googleapis.com/oauth2/v3/userinfo",
       {
@@ -77,6 +88,8 @@ const APIService =
     sessionStorage.setItem("email", data.email);
     this.fetchUserOrCreateUser(data.name, null, data.locale, "https://larrywongkahei.github.io/img/pixel_art.png", data.email)
   },
+
+  // Fetch data from github oauth api
   async FetchDataFromGithub(code) {
     const response = await fetch(`http://localhost:8080/login/github/code?code=${code}`)
     const data = await response.json()
@@ -87,6 +100,8 @@ const APIService =
     sessionStorage.setItem("email", data.email);
     this.fetchUserOrCreateUser(data.login, data.bio, data.location, data.avatar_url, data.email)
   },
+
+  // Fetch data from linkedin oauth api
   async FetchDataFromLinkedin(code) {
     const response = await fetch(`http://localhost:8080/login/linkedin/code?code=${code}`)
     const data = await response.json()
--- src/App.js
+++ src/App.js
@@ -12,11 +12,19 @@ function App() {
 
   
   useEffect(() => {
+    // get queryString (http://localhost:3000/login?id=1 to ?id=1)
     const queryString = window.location.search;
+
+    // get params value from the queryString
+    // urlParam.get("id") = 1
     const urlParam = new URLSearchParams(queryString);
+    
+    // check if urlParam has code param for github oauth login
     if(urlParam.has("code") & window.location.pathname === "/github"){
       APIService.FetchDataFromGithub(urlParam.get("code"));
     }
+
+    // check if urlParam has code param for linkedin oauth login
     else if(urlParam.has("code") & window.location.pathname === "/linkedin"){
       APIService.FetchDataFromLinkedin(urlParam.get("code"));
     }
