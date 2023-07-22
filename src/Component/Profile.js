5b4d80b44e47fa17d3a0502771a1df5cc5b5a961
Maryhei <makemak123@gmail.com>
2023-07-22 17:52:37 +0100

    Added algorithm to elements clickeable
    location to link to googlemap
    
    email open an email console

--- src/Component/Profile.js
+++ src/Component/Profile.js
@@ -16,16 +16,16 @@ export default function Profile() {
         APIService.fetchUser(sessionStorage.getItem("email")).then(data => setUserData(data))
     }, [])
 
-    function updateData(param, data){
+    function updateData(param, data) {
         APIService.updateUser(sessionStorage.getItem('userId'), param, data);
         window.location.reload();
     }
 
-    function phoneNumberHandler(e){
+    function phoneNumberHandler(e) {
         setPhoneNumber(e.target.value);
     }
 
-    function locationHandler(e){
+    function locationHandler(e) {
         setLocation(e.target.value);
     }
 
@@ -36,17 +36,17 @@ export default function Profile() {
                 <p className='name'>{sessionStorage.getItem("name")}</p>
                 <table className=''>
                     <tr>
-                        <td><IoMdMail className='icons' /></td>
-                        <td><p>{userData?.email}</p></td>
+                        <td><IoMdMail className='profileIcons' /></td>
+                        <td><a href={`mailto:${sessionStorage.getItem("email")}`} className='clickableContact'><p>{userData?.email}</p></a></td>
                     </tr>
                     <tr>
-                        <td><BsFillTelephoneFill className='icons' /></td>
+                        <td><MdLocationOn className='profileIcons' /></td>
+                        <td>{userData?.location ? <a href={`https://www.google.com/maps/search/?api=1&query=${userData.location}`} className='clickableContact'><p>{userData?.location.replace(/["]/g, "")}</p></a> : <div><input type='text' placeholder='Location' className='detailValue' value={location} onChange={locationHandler} /><GiConfirmed className={location ? "tick" : "untouchableTick"} onClick={() => updateData('location', location)} /></div>}</td>
+                    </tr>
+                    <tr>
+                        <td><BsFillTelephoneFill className='profileIcons' /></td>
                         {/* remove all the double quote with  (/["]/g, "")*/}
-                        <td>{userData?.phoneNumber ? <p>{userData?.phoneNumber.replace(/["]/g, "")}</p> : <div className='inputDetail'><input type='text' placeholder='Phone Number' className='detailValue' value={phoneNumber} onChange={phoneNumberHandler}/><GiConfirmed className={phoneNumber ? "tick" : "untouchableTick"} onClick={() => updateData("phonenumber", phoneNumber)}/></div>}</td>
-                    </tr>
-                    <tr>
-                        <td><MdLocationOn className='icons' /></td>
-                        <td>{userData?.location ? <p>{userData?.location}</p> : <div><input type='text' placeholder='Location' className='detailValue' value={location} onChange={locationHandler}/><GiConfirmed className={location ? "tick" : "untouchableTick"} onClick={() => updateData('location', location)}/></div>}</td>
+                        <td>{userData?.phoneNumber ? <p>{userData?.phoneNumber.replace(/["]/g, "")}</p> : <div className='inputDetail'><input type='text' placeholder='Phone Number' className='detailValue' value={phoneNumber} onChange={phoneNumberHandler} /><GiConfirmed className={phoneNumber ? "tick" : "untouchableTick"} onClick={() => updateData("phonenumber", phoneNumber)} /></div>}</td>
                     </tr>
                 </table>
             </div>
