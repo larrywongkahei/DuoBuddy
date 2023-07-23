cd1f4e8808d73fbc946616ec2b549eda04494238
Maryhei <makemak123@gmail.com>
2023-07-23 15:51:56 +0100

    Imported API Service

--- src/Component/ProfilePageView.js
+++ src/Component/ProfilePageView.js
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom"
 import { useEffect } from "react";
+import APIService from "../APIService";
 
 export default function ProfilePageView(){
+    useEffect(() => {
+        
+    }, [])
     const param = useParams();
     const id = param.id;
     return (
