9a4afab57469461f497d8ef716f73a69ad60888f
Maryhei <makemak123@gmail.com>
2023-07-23 12:08:41 +0100

    Manual tested the page

--- src/Component/ProfilePageView.js
+++ src/Component/ProfilePageView.js
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom"
 
 export default function ProfilePageView(){
-    
+    const param = useParams();
+    const id = param.id;
     return (
         <h1>
             this is profile viewing page + { id }
