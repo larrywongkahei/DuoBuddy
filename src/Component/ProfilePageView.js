99914ef99b5c63bdf7aa0991f0efb45353d63f76
Maryhei <makemak123@gmail.com>
2023-07-23 12:07:55 +0100

    Added to routes and imported use param to test if the id getting fine

--- src/Component/ProfilePageView.js
+++ src/Component/ProfilePageView.js
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom"
+
 export default function ProfilePageView(){
+    
     return (
         <h1>
-            this is profile viewing page.
+            this is profile viewing page + { id }
         </h1>
     )
 }
