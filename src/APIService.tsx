67e2a1e4824fcfd2b0722ad2301112d08149a28d
Maryhei <makemak123@gmail.com>
2023-07-24 16:34:34 +0100

    Converted APIServeice file to tsx from jsx

--- src/APIService.tsx
+++ src/APIService.tsx
@@ -1,7 +1,7 @@
-const APIService =
+export const APIService =
 {
   // Add user to database
-  createUser(name, bio, location, avatar, email, password) {
+  createUser(name:string, bio:string | null, location:string | null, avatar:string, email:string | null, password:string | null) {
     return fetch("http://localhost:8080/user",
       {
         method: "POST",
@@ -14,14 +14,14 @@ const APIService =
   },
 
   // Handle signup
-  async signup(name, email, password){
+  async signup(name:string, email:string, password:string){
     sessionStorage.setItem("name", name)
     sessionStorage.setItem("email", email)
     sessionStorage.setItem("avatar_url", "https://larrywongkahei.github.io/img/pixel_art.png")
     this.createUser(name, null, null, "https://larrywongkahei.github.io/img/pixel_art.png", email, password)
   },
 
-  async signin(email, password){
+  async signin(email:string, password:string){
     const fetchdata = await fetch("http://localhost:8080/login",{
       method:"POST",
       body:JSON.stringify({
@@ -47,7 +47,7 @@ const APIService =
   },
 
   // Check if user in the database, if not, Create user
-  async fetchUserOrCreateUser(name, bio, location, avatar_url, email, password) {
+  async fetchUserOrCreateUser(name:string, bio:string | null, location:string | null, avatar_url:string, email:string | null, password:string | null) {
     const fetchdata = await fetch(`http://localhost:8080/user/dataFetching?email=${email}`)
     if (fetchdata.status === 200) {
       const data = await fetchdata.json()
@@ -60,7 +60,7 @@ const APIService =
   },
 
   // Post project to database
-  createProject(payload) {
+  createProject(payload:Record<string, string>) {
     return fetch("http://localhost:8080/project",
       {
         method: "POST",
@@ -71,7 +71,7 @@ const APIService =
 
   // Update user detail, input bio, location or projects in param field
   // for project, input project id into the body
-  updateUser(userId, param, payload) {
+  updateUser(userId:string, param:string, payload:string) {
     return fetch(`http://localhost:8080/user/${userId}/${param}`,
       { method: "PUT", body: JSON.stringify(payload), headers: { 'Content-Type': 'application/json' } })
       .then(data => data.json())
@@ -82,22 +82,22 @@ const APIService =
       .then(response => response.json())
   },
 
-  getProjectById(id) {
+  getProjectById(id:string) {
     return fetch(`http://localhost:8080/project/${id}`)
       .then(response => response.json())
   },
   // searchData could be tags or title
-  getProjectsBySearch(searchData) {
+  getProjectsBySearch(searchData:string) {
     return fetch(`http://localhost:8080/project/getSearchResult?searchData=${searchData}`)
       .then(response => response.json())
   },
-  getUserById(id) {
+  getUserById(id:string) {
     return fetch(`http://localhost:8080/user/dataFetching?id=${id}`)
       .then(response => response.json())
   },
 
   // Add comment to project
-  commentProject(projectId, userId, content) {
+  commentProject(projectId:string, userId:string, content:string) {
     return fetch(`http://localhost:8080/project/${projectId}?userId=${userId}`, {
       method: "PUT",
       body: content,
@@ -106,7 +106,7 @@ const APIService =
   },
 
   // Fetch data from google oauth api
-  async FetchDataFromGoogle(token) {
+  async FetchDataFromGoogle(token:string) {
     const response = await fetch("https://www.googleapis.com/oauth2/v3/userinfo",
       {
         method: "GET",
@@ -120,7 +120,7 @@ const APIService =
   },
 
   // Fetch data from github oauth api
-  async FetchDataFromGithub(code) {
+  async FetchDataFromGithub(code:string) {
     const response = await fetch(`http://localhost:8080/login/github/code?code=${code}`)
     const data = await response.json()
     sessionStorage.setItem("name", data.login);
@@ -132,7 +132,7 @@ const APIService =
   },
 
   // Fetch data from linkedin oauth api
-  async FetchDataFromLinkedin(code) {
+  async FetchDataFromLinkedin(code:string) {
     const response = await fetch(`http://localhost:8080/login/linkedin/code?code=${code}`)
     const data = await response.json()
     sessionStorage.setItem("name", data.localizedFirstName + " " + data.localizedLastName);
@@ -141,5 +141,3 @@ const APIService =
     this.fetchUserOrCreateUser(data.localizedFirstName + " " + data.localizedLastName, null, null, "https://larrywongkahei.github.io/img/pixel_art.png", data.email, null)
   },
 }
-
-module.exports = APIService;
