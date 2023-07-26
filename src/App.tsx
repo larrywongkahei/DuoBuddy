6032684d76bb3f6d6b838a7f9f31504544f8112d
Maryhei <makemak123@gmail.com>
2023-07-26 20:35:07 +0100

    Modified the redirect uri to be duobuddy

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App:React.FC = () => {
       sessionStorage.setItem("userId", data.id);
       sessionStorage.setItem("email", data.email);
       sessionStorage.setItem("avatar_url", data.avatarUrl);
-      window.location.href = "https://larrywongkahei.github.io/MentorShip/";
+      window.location.href = "https://larrywongkahei.github.io/DuoBuddy/";
     })
     
     // check if urlParam has code param for github oauth login
