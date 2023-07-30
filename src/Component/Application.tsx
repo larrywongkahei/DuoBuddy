ca94a67212bb99b0f354ec1c8a9b826c8bd158c7
Maryhei <makemak123@gmail.com>
2023-07-30 14:38:50 +0100

    Finished coding a simple form

--- src/Component/Application.tsx
+++ src/Component/Application.tsx
@@ -5,11 +5,22 @@ const Application : React.FC = () => {
 
     const [dataToPass, setDataToPass] = useState<string>("");
 
+    function dataToPassHandler(e:React.ChangeEvent<HTMLInputElement>){
+        setDataToPass(e.target.value)
+    }
+
+    function handleSubmitHandler(e:React.MouseEvent){
+        e.preventDefault();
+        console.log(dataToPass);
+    }
+
     return (
         <form>
             <label>
                 Input your experience
             </label>
+            <input type="text" value={dataToPass} onChange={dataToPassHandler} />
+            <input type="submit" value="submit" onClick={handleSubmitHandler}/>
         </form>
     )
 }
