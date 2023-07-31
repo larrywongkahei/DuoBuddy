6a058ee439acb5617f41db79554d969ddd3ab5ba
Maryhei <makemak123@gmail.com>
2023-07-31 20:53:50 +0100

    Finished coding basic all applicant css

--- src/Component/AllApplicant.tsx
+++ src/Component/AllApplicant.tsx
@@ -1,10 +1,32 @@
 import "./AllApplicantCss.css"
-
-const AllApplicant:React.FC = () => {
+/*  Bio, project count, name, email, location, phonenumber, contacts */
+let bio = "fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk";
+let projectCount = 3;
+let name = "wong ka hei";
+let email = "makemak123@sww.com";
+let location = "Glasgow";
+let phoneNumber = "32311232";
+let contact = ["fasefasef", "fasefaesfa"]
+const AllApplicant: React.FC = () => {
     return (
-        <h1>
-            This is allapplication page
-        </h1>
+        <div className="AllApplicantContainer">
+            <div className="eachApplicantProfileData">
+                <div className="eachApplicantImage">
+                    <img src="fasefaesf" />
+                </div>
+                <div className="eachApplicationContact">
+                    {contact}
+                </div>
+                <div className="eachApplicationBio">
+                <p className="eachApplicationBioP">
+                    {bio}
+                    </p>
+                </div>
+                <button className="eachApplicationButton">
+                    to profile
+                </button>
+            </div>
+        </div>
     )
 }
 
