d1d5953c0e7b59390c2375aa742337f3892fbc30
Maryhei <makemak123@gmail.com>
2023-08-01 20:03:29 +0100

    Added each user node to test

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -53,7 +53,7 @@ export const AppRoutes = [
         element:<ProfilePageView />
     },
     {
-        path:"/applicants",
+        path:"/applicants/:id",
         element:<AllApplicant />,
     }
 
--- src/Component/AllApplicant.tsx
+++ src/Component/AllApplicant.tsx
@@ -2,6 +2,10 @@ import "./AllApplicantCss.css"
 import { useState, useEffect } from "react";
 import { User } from "./Interface";
 import { useParams } from "react-router-dom";
+import { APIService } from "../APIService";
+import { ImLinkedin } from 'react-icons/im';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { BsGithub } from 'react-icons/bs';
 /*  Bio, project count, name, email, location, phonenumber, contacts */
 let bio = "fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk fasfh asjehfaljksefjkah lsejkfh asljkehf askjlehf akjlshef lakshefajklshefjaks ehfljak shefjk";
 let projectCount = 3;
@@ -10,32 +14,57 @@ let email = "makemak123@sww.com";
 let location = "Glasgow";
 let phoneNumber = "32311232";
 let contact = ["fasefasef", "fasefaesfa"]
-const AllApplicant: React.FC<string[]>= ( userIds ) => {
+const AllApplicant: React.FC= () => {
 
     const [userDatas, setUserDatas] = useState<User[]>();
+    const params = useParams();
+    const id = params.id;
 
-    useEffect(() => {
-        
-    })
-
-    return (
-        <div className="AllApplicantContainer">
-            <div className="eachApplicantProfileData">
+    const Contacts = userDatas?.map((each, index) => {
+            return (
+                <div key={index}>
+                    <>
+                    {Object.keys(each?.contact || {}).includes('github') ? <BsGithub className='chosenAddContactIcons' onClick={() => window.location.href = each?.contact['github'] || ""} /> : null}
+                    </>
+                    <>
+                    {Object.keys(each?.contact || {}).includes('linkedin') ? <ImLinkedin className='chosenAddContactIcons' onClick={() => window.location.href = each?.contact['linkedin'] || ""} /> : null}
+                    </>
+                    <>
+                    {Object.keys(each?.contact || {}).includes('twitter') ? <AiOutlineTwitter className='chosenAddContactIcons' onClick={() => window.location.href = each?.contact['twitter'] || ""} /> : null}
+                    </>
+                </div>
+            )
+        })
+    const usersNode = userDatas?.map((each, index) => {
+        return (
+            <div className="eachApplicantProfileData" key={index}>
                 <div className="eachApplicantImage">
-                    <img src="fasefaesf" />
+                    <img src={each.avatarUrl} alt="logo"/>
                 </div>
                 <div className="eachApplicationContact">
-                    {contact}
-                </div>
+                    {Contacts}
+                </div> 
                 <div className="eachApplicationBio">
                 <p className="eachApplicationBioP">
-                    {bio}
+                    {each?.bio}
                     </p>
                 </div>
                 <button className="eachApplicationButton">
                     to profile
                 </button>
             </div>
+        )
+    })
+
+    useEffect(() => {
+        if(id){
+            APIService.getUsersByProjectIds(id).then((data:User[]) => setUserDatas(data));
+        }
+    },[])
+
+    return (
+        <div className="AllApplicantContainer">
+            {usersNode}
         </div>
     )
 }
