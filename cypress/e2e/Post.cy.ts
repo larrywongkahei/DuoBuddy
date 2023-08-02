61cbe76bc0139252485d177b3c950ade0b9c7d59
Maryhei <makemak123@gmail.com>
2023-08-02 21:31:56 +0100

    Modified reload comment p to be a button and finishing the comment test

--- cypress/e2e/Post.cy.ts
+++ cypress/e2e/Post.cy.ts
@@ -2,46 +2,46 @@
 //     beforeEach(() => {
 //         cy.visit('https://larrywongkahei.github.io/DuoBuddy/')
 //     })
-    // it('should be able to search for post by name or tags in home page', () => {
-    //     cy.get(".searchInput").type("a").type('{enter}').url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/searchResult/a')
-    //     cy.go('back').get(".searchInput").type("d").type('{enter}').url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/searchResult/d')
-    // })
-    // it('project should be able to be searched by name or tag', () => {
-    //     cy.get(".searchInput").type("python").type('{enter}').url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/searchResult/python')
-    //     cy.get(".searchResultProjects").should('have.length', 0)
-    //     cy.get(".searchPageInput").type("java").type('{enter}').url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/searchResult/java')
-    //     cy.get(".searchResultProjects").should('have.length', 1)
-    //     cy.get(".searchPageInput").type("duo").type('{enter}').url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/searchResult/duo')
-    //     cy.get(".searchResultProjects").should('have.length', 1)
-    // })
-    // it('should be able to navigate to project by searching or explore', () => {
-    //     cy.contains('Explore').click().wait(1000)
-    //     cy.get(".titleLink").should('exist')
-    //     cy.go('back')
-    //     cy.get(".searchInput").type("java").type('{enter}')
-    //     cy.get(".titleLink").should('exist').click()
-    //     cy.url().should('contain', "https://larrywongkahei.github.io/DuoBuddy/#/post")
-    //     cy.get(".ProjectPageTitle").should('exist')
-    //     cy.get(".ProjectPageHeaderDetail").should('exist')
-    //     cy.get(".mainContent").should('exist')
-    // })
-    // it('should not be able to comment when not logged in', () => {
-    //     cy.on('window:alert', (text) => {
-    //         expect(text).equal("Log in first")
-    //     })
-    //     cy.contains('Explore').click().wait(1000)
-    //     cy.get(".titleLink").click()
-    //     cy.get(".CommentBox").type("Test Comment")
-    //     cy.get(`[type = 'submit']`).click()
-    // })
-    // it('should be able to post a post if not logged in', () => {
-    //     cy.on('window:alert', (text) => {
-    //         expect(text).equal("Sign in first")
-    //     })
-    //     cy.contains('Explore').click()
-    //     cy.get('.postButton').click()
-    //     cy.url().should('equal', "https://larrywongkahei.github.io/DuoBuddy/#/explore")
-    // })
+//     it('should be able to search for post by name or tags in home page', () => {
+//         cy.get(".searchInput").type("a").type('{enter}').url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/searchResult/a')
+//         cy.go('back').get(".searchInput").type("d").type('{enter}').url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/searchResult/d')
+//     })
+//     it('project should be able to be searched by name or tag', () => {
+//         cy.get(".searchInput").type("python").type('{enter}').url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/searchResult/python')
+//         cy.get(".searchResultProjects").should('have.length', 0)
+//         cy.get(".searchPageInput").type("java").type('{enter}').url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/searchResult/java')
+//         cy.get(".searchResultProjects").should('have.length', 1)
+//         cy.get(".searchPageInput").type("duo").type('{enter}').url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/searchResult/duo')
+//         cy.get(".searchResultProjects").should('have.length', 1)
+//     })
+//     it('should be able to navigate to project by searching or explore', () => {
+//         cy.contains('Explore').click().wait(1000)
+//         cy.get(".titleLink").should('exist')
+//         cy.go('back')
+//         cy.get(".searchInput").type("java").type('{enter}')
+//         cy.get(".titleLink").should('exist').click()
+//         cy.url().should('contain', "https://larrywongkahei.github.io/DuoBuddy/#/post")
+//         cy.get(".ProjectPageTitle").should('exist')
+//         cy.get(".ProjectPageHeaderDetail").should('exist')
+//         cy.get(".mainContent").should('exist')
+//     })
+//     it('should not be able to comment when not logged in', () => {
+//         cy.on('window:alert', (text) => {
+//             expect(text).equal("Log in first")
+//         })
+//         cy.contains('Explore').click().wait(1000)
+//         cy.contains('test input 1').click()
+//         cy.get(".CommentBox").type("Test Comment")
+//         cy.get(`[type = 'submit']`).click()
+//     })
+//     it('should not be able to post a post if not logged in', () => {
+//         cy.on('window:alert', (text) => {
+//             expect(text).equal("Sign in first")
+//         })
+//         cy.contains('Explore').click()
+//         cy.get('.postButton').click()
+//         cy.url().should('equal', "https://larrywongkahei.github.io/DuoBuddy/#/explore")
+//     })
 // })
 describe('Test about posts that require loggin', () => {
     beforeEach(() => {
@@ -53,53 +53,55 @@ describe('Test about posts that require loggin', () => {
         cy.intercept('https://larrywongkahei.github.io/DuoBuddy/').as('login')
         cy.wait('@login')
     })
-        // it('should be able to comment if logged in', () => {
+        it('should be able to comment if logged in', () => {
+        cy.contains('Explore').click()
+        cy.get(':nth-child(1) > .titleLink > h1').click()
+        const comments = cy.get('.eachComment').its('length')
+        cy.log(comments)
+        cy.get(".CommentBox").type("Test Comment")
+        cy.get(`[type = 'submit']`).click()
+        cy.get('.CommentSessionContainer > p').click().wait(5000)
+        cy.get('.eachComment').its('length').then((length) => {
+            cy.log(length)
+            comments.should('equal', length - 1)
+        })
+    })
+    // it('should be able to post a post if logged in', () => {
     //     cy.contains('Explore').click()
-    //     cy.get(".titleLink").click()
-    //     const comments = cy.get('.eachComment').its('length')
-    //     cy.get(".CommentBox").type("Test Comment")
-    //     cy.get(`[type = 'submit']`).click()
-    //     cy.get('#loadPageButton').click().wait(1000)
-    //     cy.get('.eachComment').its('length').then((length) => {
-    //         comments.should('equal', length)
-    //     })
+    //     cy.get('.postButton').click()
+    //     cy.url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/idea/post')
+    //     cy.get('.inputAndButton > input').type("test input 1")
+    //     cy.get('.inputAndButton > button').click()
+    //     cy.get('.introduceYourselfContainer > .inputAndButton > textarea').type("test input 2")
+    //     cy.get('.introduceYourselfContainer >.inputAndButton > button').click()
+    //     cy.get('.ideaDetailContainer > .inputAndButton > textarea').type("test input 3")
+    //     cy.get('.ideaDetailContainer > .inputAndButton> button').click()
+    //     cy.get('.explainInspiration > .inputAndButton > textarea').type("test input 4")
+    //     cy.get('.explainInspiration > .inputAndButton> button').click()
+    //     cy.get('.others > .inputAndButton > textarea').type("test input 5")
+    //     cy.get('.others > .inputAndButton> button').click()
+    //     cy.get('.tagsInput').type("tag1")
+    //     cy.get('.addTagsContainer > button').click()
+    //     cy.get('.tagsInput').type("tag2")
+    //     cy.get('.addTagsContainer > button').click()
+    //     cy.get('.nextPage').click()
+    //     cy.url().should('not.equal', 'https://larrywongkahei.github.io/DuoBuddy/#/idea/post')
+    //     cy.get(".ProjectPageTitle").should('exist')
+    //     cy.get(".ProjectPageHeaderDetail").should('exist')
+    //     cy.get(".mainContent").should('exist')
     // })
-    it('should be able to post a post if logged in', () => {
-        cy.contains('Explore').click()
-        cy.get('.postButton').click()
-        cy.url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/idea/post')
-        cy.get('.inputAndButton > input').type("test input 1")
-        cy.get('.inputAndButton > button').click()
-        cy.get('.introduceYourselfContainer > .inputAndButton > textarea').type("test input 2")
-        cy.get('.introduceYourselfContainer >.inputAndButton > button').click()
-        cy.get('.ideaDetailContainer > .inputAndButton > textarea').type("test input 3")
-        cy.get('.ideaDetailContainer > .inputAndButton> button').click()
-        cy.get('.explainInspiration > .inputAndButton > textarea').type("test input 4")
-        cy.get('.explainInspiration > .inputAndButton> button').click()
-        cy.get('.others > .inputAndButton > textarea').type("test input 5")
-        cy.get('.others > .inputAndButton> button').click()
-        cy.get('.tagsInput').type("tag1")
-        cy.get('.addTagsContainer > button').click()
-        cy.get('.tagsInput').type("tag2")
-        cy.get('.addTagsContainer > button').click()
-        cy.get('.nextPage').click()
-        cy.url().should('not.equal', 'https://larrywongkahei.github.io/DuoBuddy/#/idea/post')
-        cy.get(".ProjectPageTitle").should('exist')
-        cy.get(".ProjectPageHeaderDetail").should('exist')
-        cy.get(".mainContent").should('exist')
-    })
-    it('should be able to delete a post as a creater', () => {
-        cy.on('window:confirm', (text) => 
-        {
-            expect(text).equal("Delete this post?")
-            return true
-        }
-            );
-        cy.contains('Explore').click()
-        cy.contains('test input 1').should('exist').click()
-        cy.get('.TitleAndApplyButton > button').should('contain', "Close this post").click()
-        cy.url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/explore')
-        cy.contains('test input 1').should('not.be.exist')
+    // it('should be able to delete a post as a creater', () => {
+    //     cy.on('window:confirm', (text) => 
+    //     {
+    //         expect(text).equal("Delete this post?")
+    //         return true
+    //     }
+    //         );
+    //     cy.contains('Explore').click()
+    //     cy.contains('test input 1').should('exist').click()
+    //     cy.get('.TitleAndApplyButton > button').should('contain', "Close this post").click()
+    //     cy.url().should('equal', 'https://larrywongkahei.github.io/DuoBuddy/#/explore')
+    //     cy.contains('test input 1').should('not.be.exist')
 
-    })
+    // })
 })
