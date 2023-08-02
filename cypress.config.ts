d00e263fc8cd06177e011f73451507957128c348
Maryhei <makemak123@gmail.com>
2023-08-03 00:22:43 +0100

    Finsihed coding the login test and uncomment all test codes

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    "chromeWebSecurity": false
+    "chromeWebSecurity": false,
+    "experimentalRunAllSpecs": true
   },
 });
